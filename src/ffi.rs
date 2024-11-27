f39840806e78c164e69d2beb09759f6841ecf46e
This is AijoKaren <90033092+misaka19683@users.noreply.github.com>
2024-11-27 12:49:44 +0800

    Update ffi.rs

--- src/ffi.rs
+++ src/ffi.rs
@@ -23,8 +23,8 @@ pub struct nfgenmsg;
 #[repr(C)]
 pub struct nfq_data;
 
-#[repr(C)]
-#[packed]
+///#[repr(C)]
+#[repr(C, packed)]
 /// The NFQueue specific packet data
 pub struct nfqnl_msg_packet_hdr {
     /// The packet id
